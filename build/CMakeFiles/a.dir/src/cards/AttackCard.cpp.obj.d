4a702d316534fc1f32f6eab193d866735c82724f
*mahdi* <*mahdidehnavi0086@gmail.com*>
2026-07-06 14:35:14 +0330

    fix : slove bug for controller

---
+++ build/CMakeFiles/a.dir/src/cards/AttackCard.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/a.dir/src/cards/AttackCard.cpp.obj: \
+ C:/Users/Parsumash/Desktop/folders\ (1)/project/src/cards/AttackCard.cpp
---
+++ build/CMakeFiles/a.dir/src/cards/DefenseCard.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/a.dir/src/cards/DefenseCard.cpp.obj: \
+ C:/Users/Parsumash/Desktop/folders\ (1)/project/src/cards/DefenseCard.cpp

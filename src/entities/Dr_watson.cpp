95cfb72ca1bf66a9058aa8f1418d13694895176b
*mahdi* <*mahdidehnavi0086@gmail.com*>
2026-07-07 12:20:46 +0330

    feat : add ability for vaston and sister

--- src/entities/Dr_watson.cpp
+++ src/entities/Dr_watson.cpp
@@ -11,7 +11,8 @@ Dr_watson::Dr_watson(): Character("Dr_watson", 8 , 2 , 0)
 
 void Dr_watson::ability()
 {
-    
+    cout << "\n " << getName() << " has no special ability." << endl;
+    cout << "   Dr. Watson is Sherlock's loyal companion and fights alongside him." << endl;
 }
 
 Dr_watson::~Dr_watson()
